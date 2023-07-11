6858046192b87eba8d9eb273e6e017b60fc72f7d
Lucas Andreussi <lucasandreussi@gmail.com>
2023-07-11 17:13:10 -0300

    feat(stout): add new host

---
+++ hosts/stout/configuration.nix
@@ -0,0 +1,92 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+      ./hardware-configuration.nix
+      ./home.nix
+  ];
+
+  ########## Boot ##########
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  ########## Networking ##########
+  networking.hostName = "stout"; 
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  ########## TZ ##########
+  time.timeZone = "America/Sao_Paulo";
+
+  ########## Console ##########
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; 
+  };
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  ########## Video ##########
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  services.xserver = {
+    enable = true;
+    xkbVariant = "intl";
+
+    # displayManager.startx.enable = true;
+    windowManager.i3 = { 
+      enable = true;
+      configFile = ../../programs/i3/config;
+    };
+    xrandrHeads = [{output = "HDMI-2"; primary = true;}];
+  };
+
+  ########## Sound ##########
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  ########## Bluetooth ##########
+  hardware.bluetooth.enable = true;
+
+  ########## Home ##########
+
+  ########## Global Programs ##########
+  environment.systemPackages = with pkgs; [
+    curl
+    coreutils
+    lxappearance
+    xdg_utils
+    xdg-user-dirs
+    xclip
+    wget
+  ];
+  programs.gnupg.agent.enable = true;
+
+  ########## Fonts ##########
+  fonts = {
+    enableDefaultFonts = true;
+    fontDir.enable = true;
+    fontconfig = {
+      useEmbeddedBitmaps = true;
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" ];
+	serif = [ "Noto Serif" ];
+	sansSerif = [ "Noto Sans" ];
+      };
+   };
+
+   fonts = with pkgs; [
+     font-awesome
+     lmodern
+     noto-fonts
+     noto-fonts-cjk
+     noto-fonts-emoji
+     (nerdfonts.override { fonts = [ "JetBrainsMono"]; })
+   ];
+ };
+
+  ########## Nixpkgs version ##########
+  system.stateVersion = "23.05";
+}
+
---
+++ hosts/stout/home.nix
@@ -0,0 +1,70 @@
+super@{ pkgs, config, ... }:
+
+{
+  imports = [ <home-manager/nixos> ];
+
+  home-manager.users.landreussi = {
+    home = {
+      username = "landreussi";
+      homeDirectory = "/home/landreussi";
+      packages = with pkgs; [
+        jq
+        git
+        brave
+	nixfmt
+        htop
+        neofetch
+        blueman
+	spotify-tui
+	lazygit
+	fd
+	fzf
+	go
+	gcc
+	pass
+	tree
+  pinentry
+      ];
+
+      file = {
+        neovim = {
+          source = ../../programs/neovim/nvchad;
+          target = "/home/landreussi/.config/nvim";
+	  recursive = true;
+        };
+        i3status = {
+          source = ../../programs/i3status;
+          target = "/home/landreussi/.config/i3status";
+        };
+      };
+
+      stateVersion = "23.05";
+    };
+    xdg = {
+      enable = true;
+      userDirs.enable = true;
+    }; 
+
+    manual.manpages.enable = false;
+    programs.fish = import ../../programs/fish.nix super;
+    programs.git = import ../../programs/git.nix super // {
+      extraConfig.core.sshCommand = "ssh -i ~/.ssh/stout";
+    };
+    programs.gpg = import ../../programs/gpg.nix super;
+    programs.kitty = import ../../programs/kitty.nix super;
+    programs.neovim = import ../../programs/neovim.nix super;
+    programs.ssh = import ../../programs/ssh.nix super // {
+      matchBlocks.identityFile =
+        "${config.users.users.landreussi.home}/.ssh/stout";
+    };
+    programs.tmux = import ../../programs/tmux.nix super;
+    programs.home-manager.enable = true;
+  };
+
+  users.users.landreussi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; 
+    name = "landreussi";
+    home = "/home/landreussi";
+  };
+}
